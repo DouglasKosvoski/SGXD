ba3b16402c145d83c62024e59e1c1e2a89e140ae
Douglas Kosvoski <douglas.contactpro@gmail.com>
2021-03-13 22:07:47 -0300

    Pause Menu Complete

--- Scripts/PauseMenu.gd
+++ Scripts/PauseMenu.gd
@@ -1,12 +1,17 @@
 extends Container
 
 func _on_PauseButton_pressed():
-	$CenterContainer.visible = true
+	$Content.visible = true
+	$Background.visible = true
 	$PauseButton.visible = false
+	get_tree().paused = true
 	
 func _on_ResumeButton_pressed():
-	$CenterContainer.visible = false
+	$Content.visible = false
+	$Background.visible = false	
 	$PauseButton.visible = true
-
+	get_tree().paused = false
+	
 func _on_QuitButton_pressed():
+	get_tree().paused = false
 	get_tree().quit()
