81252a7273860fee1ce84970a8ab4b3f6391d93a
Douglas Kosvoski <douglas.contactpro@gmail.com>
2021-04-16 14:51:34 -0300

    Implemented run animation... self detects the current level and select the animations set accordingly

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -16,34 +16,50 @@ const UP = Vector2(0,-1)
 const joystick_trigger = 0.2
 var motion = Vector2()
 var vacine_counter = 0
+var anim
+
+func _ready():
+	if current_level == 1:
+		anim = $Kid
+	elif current_level == 2:
+		anim = $Teen
+	elif current_level == 3:
+		anim = $Adult
+	anim.visible = true
 
 func _physics_process(delta):
+	animation_control()
+	flip_animation()
+	motion = move_and_slide(motion)
+
+func animation_control():
 	# Horizontal
 	if Input.is_action_pressed("ui_right") or joystick.get_value().x > joystick_trigger:
 		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+		anim.play("run")
 	elif Input.is_action_pressed("ui_left") or joystick.get_value().x < -joystick_trigger:
 		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+		anim.play("run")
 	else:
 		motion.x = lerp(motion.x, 0, FRICTION)
-		$AnimatedSprite.play("idle")
+		anim.play("idle")
 		
 	# Vertical
 	if Input.is_action_pressed("ui_up") or joystick.get_value().y < -joystick_trigger:
 		motion.y = min(motion.y + ACCELERATION, -MAX_SPEED)
+		anim.play("run")
 	elif Input.is_action_pressed("ui_down") or joystick.get_value().y > joystick_trigger:
 		motion.y = max(motion.y - ACCELERATION, +MAX_SPEED)
+		anim.play("run")
 	else:
 		motion.y = lerp(motion.y, 0, FRICTION)
-		$AnimatedSprite.play("idle")
-
-	flip_animation()
-	motion = move_and_slide(motion)
+		anim.play("idle")
 
 func flip_animation():
 	if Input.is_action_just_pressed("ui_right") or joystick.get_value().x > 0:
-		$AnimatedSprite.flip_h = false
+		anim.flip_h = false
 	elif Input.is_action_just_pressed("ui_left") or joystick.get_value().x < 0:
-		$AnimatedSprite.flip_h = true
+		anim.flip_h = true
 
 func gravity(motion, g):
 	motion.y += g
@@ -68,3 +84,5 @@ func level_win():
 		get_tree().change_scene(level2)
 	elif current_level == 2:
 		get_tree().change_scene(level3)
+	elif current_level == 3:
+		get_tree().change_scene("res://Scenes/Ending.tscn")
