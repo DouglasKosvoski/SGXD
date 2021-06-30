170f8fae1265aedcb3a03d4d1b15171d3fff86f5
Douglas Kosvoski <douglas.contactpro@gmail.com>
2021-06-30 11:12:52 -0300

    Update: Goal scene updated with new image and animation_player to fade in and out to next scene

--- Scripts/Goal.gd
+++ Scripts/Goal.gd
@@ -2,5 +2,8 @@ extends Control
 
 export(String, FILE, "*.tscn") var next_scene
 
-func _on_ContinueButton_pressed():
+func _process(delta):
+	$CanvasLayer/AnimationPlayer.play("fade")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
 	get_tree().change_scene(next_scene)
